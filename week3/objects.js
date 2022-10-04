3474ba63c90b847d0b892b1fea0c11b0e052daf4
Philippe De Pauw - Waterschoot <phildp@MACINTOSH.local>
2022-10-04 14:23:34 +0200

    Added objects car cat person

--- week3/objects.js
+++ week3/objects.js
@@ -54,7 +54,7 @@ console.log(date.toDateString());
 let v = Number.MIN_VALUE;
 console.log(v);
 v = Number.MAX_VALUE;
-console.log(v);
+console.log(v.toFixed(5));
 v = 32.567865432345675432;
 v = Number(v);
 v = v.toFixed(2);
@@ -72,3 +72,112 @@ const words = str.split(' ');
 for (let i = 0; i < words.length; i++) {
   console.log(`${words[i]}`);
 }
+
+const paragraph = 'The quick brown fox jumps over the lazy dog';
+const l = paragraph.length;
+let result = '';
+for (let i = 0; i < l; i++) {
+  result = paragraph.charAt(i) + result;
+}
+console.log(result);
+
+// Basic loops
+str = '';
+for (let j = 0; j < 1000; j++) {
+  str += j + '\t';
+}
+//console.log(str);
+
+str = '';
+for (let j = 1000; j >= 0; j--) {
+  str += j + '\t';
+}
+//console.log(str);
+
+// Zoek naar een woord in een string
+let searchTerm = 'Fox';
+const position = paragraph.toLowerCase().indexOf(searchTerm.toLowerCase());
+if (position === -1) {
+  console.log(`No results for search string ${searchTerm}.`);
+} else {
+  console.log(`We han an occurence for search string ${searchTerm} at index ${position}.`);
+}
+
+const PI = Math.PI;
+console.log(PI.toFixed(4));
+const angleDegrees = 45;
+const angleRadians = ((2*PI) / 360) * angleDegrees;
+console.log(angleRadians);
+const vSin = Math.sin(angleRadians);
+console.log(vSin);
+
+// Create a new Object with Object Literals
+const cat = {
+  age: 2,
+  amountOfLegs: 4,
+  race: 'Fergal',
+  gender: 'male',
+  lives: 7,
+  eat: (n) => {
+    if (n > 300) {
+      console.log('Beugh');
+    } else {
+      console.log('Mjammie');
+    }
+  },
+  jump(h) {
+    if (this.lives - 1 < 0){
+      console.log('You are Dead');
+    }
+    else if (h > 10) {
+      this.lives--;
+      console.log(`${this.lives} lives left`);
+    } else {
+      console.log('Nothing broken');
+    }
+  },
+};
+cat.eat(400);
+cat.jump(1);
+cat.jump(20);
+cat.jump(20);
+cat.jump(20);
+cat.jump(20);
+cat.jump(20);
+cat.jump(20);
+cat.jump(20);
+cat.jump(20);
+
+// Describe a new Car via object literal
+const car = {
+  'brand': 'Hyundai',
+  'amountOfFuel': 20,
+  isAutomatic: false,
+  engine: {
+    cylinders: 6,
+    hoursePower: 305,
+  },
+  toString() {
+    return `${this.brand}`;
+  },
+  tune() {
+    this.engine.cylinders++;
+    this.hoursePower = 305 + (this.engine.cylinders - 7) * 20;
+  }
+};
+console.log(car.toString());
+car.engine.cylinders = 7;
+console.log(car);
+car.tune();
+console.log(car);
+car.tune();
+car.tune();
+car.tune();
+car.tune();
+car.tune();
+car.tune();
+console.log(car);
+
+const car1 = car;
+car1.color = 'gray';
+console.log(car);
