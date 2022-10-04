9a77b4e350c27d8491b9d0496d591775db8fb999
Philippe De Pauw - Waterschoot <phildp@MACINTOSH.local>
2022-10-04 15:23:35 +0200

    Added emojis in questions

---
+++ week3/questions.js
@@ -0,0 +1,27 @@
+const windDirection = 'E'; // N, E, S, W
+let msg = 'The wind comes from direction ';
+switch (windDirection) {
+  case 'N': msg += 'North'; break;
+  case 'E': msg += 'East'; break;
+  case 'S': msg += 'South'; break;
+  case 'W': default: msg += 'West'; break;
+}
+console.log (msg);
+
+// Unicode
+const avatar = '😀'; // U+1F600
+let code = avatar.codePointAt(0);
+console.log(code); // 128512
+let hStr = code.toString(16);
+console.log(hStr); // 1f600
+code = avatar.codePointAt(1);
+console.log(code); // 56832
+hStr = '\u{1f600}';
+console.log(hStr);
+
+// Do some silly stuff with unicode
+let output = '';
+for (let k = 128512; k < 129000; k++) {
+  output += String.fromCodePoint(k);
+}
+console.log(output);
