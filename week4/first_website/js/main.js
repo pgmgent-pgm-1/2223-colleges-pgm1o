f93f2622538fcfe59743d30ad442ad2d31214217
Philippe De Pauw - Waterschoot <phildp@MACINTOSH.local>
2022-10-14 15:11:30 +0200

    Added first DOM example => super NICE

---
+++ week4/first_website/js/main.js
@@ -0,0 +1,29 @@
+// console.log('Hello World');
+
+// console.log(document);
+// const title = document.title;
+// console.log(title);
+// document.title = 'My Portfolio Website';
+
+// const $container = document.querySelector('.container');
+// console.log($container);
+// const txt = $container.innerText;
+// console.log(txt);
+// $container.innerText = 'WWW';
+
+// function tick() {
+//   const date = new Date();
+//   const timeStr = date.toTimeString().split(' ')[0];
+//   $container.innerText = timeStr;
+//   $container.style.transform = `rotate(${date.getMilliseconds()*6}deg) scale(${date.getSeconds()/6})`;
+// }
+// setInterval(() => {
+//   tick();
+// }, 1);
+
+const $container = document.querySelector('.container');
+let tempStr = '';
+for (let i=0; i < 360; i++) {
+  tempStr += `<div class="bit" style="background:hsl(${i},50%,50%)">BIT</div>`;
+}
+$container.innerHTML = tempStr;
